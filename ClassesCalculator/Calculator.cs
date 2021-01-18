49ac2c2b8c14744d90dd6f3828416dd65a55bea4
Selim-Baskaya-22 <77636247+Selim-Baskaya-22@users.noreply.github.com>
2021-01-18 22:37:43 +0300

    Add files via upload

---
+++ ClassesCalculator/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesFormGnecay
+{
+    class Calculator
+    {
+       
+        public int sum(int s1,int s2)
+        {
+            
+            return s1 + s2;
+        }
+        public double sumoverride(double s1, int s2,int s3)
+        {
+
+            return s1 + s2+s3;
+        }
+        public double dividing(double s1,double s2)
+        {
+           
+            return s1 / s2;
+        }
+    }
+}
---
+++ ClassesCalculator/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClassesFormGnecay
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        Calculator calculator = new Calculator();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double s1 = Convert.ToDouble(textBox1.Text);
+            int s2 = Convert.ToInt32(textBox2.Text);
+            int s3 = Convert.ToInt32(textBox3.Text);
+            double sonuc = calculator.sumoverride(s1,s2,s3);
+            label2.Text = sonuc.ToString();
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int s1 = Convert.ToInt32(textBox1.Text);
+            int s2 = Convert.ToInt32(textBox2.Text);
+            double result=calculator.dividing(s1, s2);
+            label2.Text = result.ToString();
+        }
+    }
+}
